c4e0cdc01e4e05cca41d065de02b4a96912bd1fe
Ash Regmi <asha3488@uni.sydney.edu.au>
2021-03-20 11:55:50 +1100

    ++ Initial Commit ++
    + Inserting main.py and about to create the structure for the robot
      challenge.
    + will create the required files as I think on them.

---
+++ main.py
@@ -0,0 +1,6 @@
+import sys
+
+'''
+    This file will be called upon running the Robot Challenge task
+    The required files like creating the table, intercepting the commands will all automatically be run.
+'''
